6528a463b685250d613a4ad1d1e147126370acec
Kim Lew <kimlew451@gmail.com>
2020-01-15 10:35:58 -0700

    Changes user prompts & edits comments so is clearer what to enter

--- bash_rsync.sh
+++ bash_rsync.sh
@@ -3,45 +3,43 @@
 # Name: bash_rsync.sh
 #
 # Brief: Bash script with user prompts so user does not forget what to enter as
-# source and destination, since it is a bit tricky. Otherwise, you can easily 
-# end up with extra directories you did not want. 
-# 
+# source and destination, since it is a bit tricky. Otherwise, you can easily
+# end up with extra sub-directories you did not want.
+#
 # Author: Kim Lew
 
-# Display user prompts of what to enter.
-# Test with:  /Users/kimlew/Sites/bash_projects/test_rsync
-echo "Type the source directory path & include specific sub-directory\
-(/Users/kimlew/Documents):"
+# Display user prompts of what to enter to prevent creating extra sub-directories.
+# For the source: 
+# Enter the specific sub-directory WITHOUT /. rsync is smart enough to
+# create the sub-directory if it doesn't exist & transfers contents. 
+# If sub-directory already exists, rsync transfers contents. 
+# For the destination/target: 
+# You do NOT need to state specific sub-directory BUT you do need /.
+echo "Type source directory path WITH specific sub-directory & LEAVE OFF trailing /."
+echo "(Example: /Users/kimlew/Documents):"
 read source_path
-
-echo "Type the destination directory path but LEAVE OFF specific sub-directory\
-(/Volumes/Toshiba\ Ext\ Drv/):"
+echo "Type destination directory path WITHOUT sub-directory & WITH trailing /."
+echo "(Example: /Volumes/KINGSTON/Documents/):"
 read destination_path
 
-echo "Source path you typed is: $source_path"
-echo "Destination path you typed is: $destination_path"
-
-# TODO: Add a trailing \ to the source to ONLY copy the contents.
-# TODO: Use -update - so ONLY transfers new or changed files.
-# TODO: Delete? old files on destination directory, e.g., Git Essent Train Ch 1.
-
-# TEST directory & file: test_rsync/test_rsync.txt
+echo "Source you typed is: $source_path"
+echo "Destination you typed is: $destination_path"
 
 # Check for valid directory path for source.
 # Check for valid directory path for destination.
-if [ ! -d "$source_path" ] 
+if [ ! -d "$source_path" ]
 then
     echo "This directory does NOT exist."
     exit 1
 fi
-if [ ! -d "$destination_path" ] 
+if [ ! -d "$destination_path" ]
 then
     echo "This directory does NOT exist."
     exit 1
 fi
 
-# TODO: $source_path_contents = $source_path.\
-# TODO: rsync $source_path $destination_path
+# -a, --archive - archive mode; same as -rlptgoD (no -H). -a implies -r.
+# -v is verbose vs. -q, --quiet - to suppress non-error messages.
 
 echo "File sync in progress..."
 echo "..."
