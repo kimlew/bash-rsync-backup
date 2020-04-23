13398e357b44880781c3715e853f7d68804d8d81
Kim Lew <kimlew451@gmail.com>
2020-04-23 11:54:04 -0600

    Adds & combines functions so cleaner code, calculates # of transferred files

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -24,22 +24,33 @@
 # Check for valid directory paths for source & destination.
 # -e - tests if a path exists without testing what type of file it is.
 check_if_directory() {
-    path_passed_in=$1
+  path_passed_in=$1
 
-    if [ ! -d "$path_passed_in" ]; then
-        echo "This directory does NOT exist:" "$path_passed_in"
-        exit 1
-    fi
+  if [ ! -d "$path_passed_in" ]; then
+      echo "This directory does NOT exist:" "$path_passed_in"
+      echo "Make sure drive is plugged in."
+      exit 1
+  fi
 }
 
-source_has_files() {
+count_files() {
   path_passed_in=$1
-  number_of_files_in_src=$(find "${path_passed_in%/}" -type f | wc -l)
+  find "${path_passed_in%/}" -type f 2> /dev/null | wc -l
 
-if [ "${number_of_files_in_src}" -eq 0 ]; then
-  echo "There are NO files at this source path" "$path_passed_in"
-  exit 1
-fi
+  if [ ! -d "$path_passed_in" ]; then
+    echo "There are NO files at this path" "$path_passed_in"
+    exit 1
+  fi
+}
+
+count_files_dirs_etc() {
+  path_passed_in=$1
+  find "${path_passed_in%/}" 2> /dev/null | wc -l
+
+  if [ ! -d "$path_passed_in" ]; then
+    echo "There are NO files, directories, symlinks, etc. at this path" "$path_passed_in"
+    exit 1
+  fi
 }
 
 while true
@@ -47,14 +58,14 @@ do
 clear
 
 cat <<MENU
-BACKUP Contents from a Directory on Laptop to a Storage Device
------------------------------------------------------------------
+BACKUP the Contents from a Directory on your Laptop to a Storage Device
+-----------------------------------------------------------------------
 1. Backup laptop's Documents folder -> Red Toshiba & Blue Toshiba
 2. Backup laptop's PHOTOS folder -> Red Toshiba & Blue Toshiba
 3. Backup Black Kingston USB -> Red Toshiba
 4. Backup Black Kingston USB -> Blue Toshiba
 0. Quit
------------------------------------------------------------------
+-----------------------------------------------------------------------
 MENU
 
     # Custom user prompt. 
@@ -90,24 +101,24 @@ MENU
         check_if_directory "$source_path_Documents"
         check_if_directory "$dest_red"
         check_if_directory "$dest_blue"
-        source_has_files "$source_path_Documents"
 
-        number_of_files_in_src=$(find "${source_path_Documents%/}" -type f 2> /dev/null | wc -l)
+        number_of_files_in_src=$(count_files "$source_path_Documents")
         echo "Number of files in Documents: " "$number_of_files_in_src"
-        number_of_files_dirs_etc_in_src=$(find "${source_path_Documents%/}" 2> /dev/null | wc -l)
-        echo "Number of files, dirs, symlinks, etc. in Documents source path: " "$number_of_files_dirs_etc_in_src"
+        number_of_files_dirs_etc_in_src=$(count_files_dirs_etc "$source_path_Documents")
+        echo "Number of files, dirs, symlinks, etc. in Documents: " "$number_of_files_dirs_etc_in_src"
         echo
 
-        number_of_files_in_dest_red=$(find "${dest_red%/}" -type f 2> /dev/null | wc -l)
-        echo "BEFORE BACKUP: Number of files in RED HD dest path: " "$number_of_files_in_dest_red"
-        number_of_files_dirs_etc_in_dest_red=$(find "${dest_red%/}" 2> /dev/null | wc -l)
-        echo "BEFORE BACKUP: Number of files, dirs, symlinks, etc. BEFORE BACKUP in RED HD dest path: " "$number_of_files_dirs_etc_in_dest_red"
+        echo "BEFORE BACKUP: "
+        number_of_files_in_dest_red=$(count_files "$dest_red")
+        echo "Number of files in RED HD dest path: " "$number_of_files_in_dest_red"
+        number_of_files_dirs_etc_in_dest_red=$(count_files_dirs_etc "$dest_red")
+        echo "Number of files, dirs, symlinks, etc. in RED HD dest path: " "$number_of_files_dirs_etc_in_dest_red"
         echo
 
-        number_of_files_in_dest_blue=$(find "${dest_blue%/}" -type f 2> /dev/null | wc -l)
-        echo "BEFORE BACKUP: Number of files in BLUE HD dest path: " "$number_of_files_in_dest_blue"
-        number_of_files_dirs_etc_in_dest_blue=$(find "${dest_blue%/}" 2> /dev/null | wc -l)
-        echo "BEFORE BACKUP: Number of files, dirs, symlinks, etc., in BLUE HD dest path: " "$number_of_files_dirs_etc_in_dest_blue"
+        number_of_files_in_dest_blue=$(count_files "$dest_blue")
+        echo "Number of files in BLUE HD dest path: " "$number_of_files_in_dest_blue"
+        number_of_files_dirs_etc_in_dest_blue=$(count_files_dirs_etc "$dest_blue")
+        echo "Number of files, dirs, symlinks, etc., in BLUE HD dest path: " "$number_of_files_dirs_etc_in_dest_blue"
         echo
         
         echo "BACKUP in progress..."
@@ -118,30 +129,42 @@ MENU
         # > dry-run_Documents_to_RedHD.txt \
         rsync -avi --progress --stats --exclude={'.DocumentRevisions-V100','.TemporaryItems','.Spotlight-V100','.Trashes','.fseventsd'} \
         "$source_path_Documents" "$dest_red" \
-        | grep 'files transferred' > backup_Documents_to_RedHD.txt \
+        > backup_Documents_to_RedHD.txt \
         && echo "BACKUP DONE of Documents -> Red Toshiba."
       
         rsync -avi --progress --stats --exclude={'.DocumentRevisions-V100','.TemporaryItems','.Spotlight-V100','.Trashes','.fseventsd'} \
         "$source_path_Documents" "$dest_blue" \
-        | grep 'files transferred' > backup_Documents_to_BlueHD.txt \
+        > backup_Documents_to_BlueHD.txt \
         && echo "BACKUP DONE of Documents -> Blue Toshiba."
         echo
 
+        echo "AFTER BACKUP: "
         number_of_files_in_dest_red_after_backup=$(find "${dest_red%/}" -type f 2> /dev/null | wc -l)
-        echo "AFTER BACKUP: Number of files in RED HD dest path: " "$number_of_files_in_dest_red_after_backup"
+        echo "Number of files in RED HD dest path: " "$number_of_files_in_dest_red_after_backup"
         number_of_files_dirs_etc_in_dest_red_after_backup=$(find "${dest_red%/}" 2> /dev/null | wc -l)
-        echo "AFTER BACKUP: Number of files, dirs, symlinks, etc. in RED HD dest path: " "$number_of_files_dirs_etc_in_dest_red_after_backup"
+        echo "Number of files, dirs, symlinks, etc. in RED HD dest path: " "$number_of_files_dirs_etc_in_dest_red_after_backup"
         echo
 
         number_of_files_in_dest_blue_after_backup=$(find "${dest_blue%/}" -type f 2> /dev/null | wc -l)
-        echo "AFTER BACKUP: Number of files in BLUE HD dest path: " "$number_of_files_in_dest_blue_after_backup"
+        echo "Number of files in BLUE HD dest path: " "$number_of_files_in_dest_blue_after_backup"
         number_of_files_dirs_etc_in_dest_blue_after_backup=$(find "${dest_blue%/}" 2> /dev/null | wc -l)
-        echo "AFTER BACKUP: Number of files, dirs, symlinks, etc. in BLUE HD dest path: " "$number_of_files_dirs_etc_in_dest_blue_after_backup"
+        echo "Number of files, dirs, symlinks, etc. in BLUE HD dest path: " "$number_of_files_dirs_etc_in_dest_blue_after_backup"
         echo
         
+        transferred_files_to_red=$((number_of_files_in_dest_red_after_backup-number_of_files_in_dest_red))
+        transferred_files_to_blue=$((number_of_files_in_dest_blue_after_backup-number_of_files_in_dest_blue))
+        echo "TOTAL files transferred to RED: " "$transferred_files_to_red"
+        echo "TOTAL files transferred to BLUE: " "$transferred_files_to_blue"
+
+        transferred_files_to_red_way2=$((number_of_files_dirs_etc_in_dest_red_after_backup - number_of_files_dirs_etc_in_dest_red))
+        transferred_files_to_blue_way2=$((number_of_files_dirs_etc_in_dest_blue_after_backup - number_of_files_dirs_etc_in_dest_blue))
+        echo "TOTAL files transferred to RED way2: " "$transferred_files_to_red_way2"
+        echo "TOTAL files transferred to BLUE way2: " "$transferred_files_to_blue_way2"
+        echo
+
         time_end=$(date +%s)
         time_diff=$((time_end - time_start))
-        echo "Processing files took:" $((time_diff/60)) "min(s)" $((time_diff%60)) "sec(s)" 
+        echo "Processing Time:" $((time_diff/60)) "min(s)" $((time_diff%60)) "sec(s)" 
         echo
         break
         ;;
@@ -181,7 +204,8 @@ MENU
 
         check_if_directory "$source_path_black_usb"
         check_if_directory "$dest_red"
-        source_has_files "$source_path_black_usb"
+
+        count_files "$source_path_black_usb"
 
         number_of_files_in_src=$(find "${source_path_black_usb%/}" -type f | wc -l)
         echo "Number of files in source path: " "$number_of_files_in_src"
@@ -211,7 +235,8 @@ MENU
 
         check_if_directory "$source_path_black_usb"
         check_if_directory "$dest_blue"
-        source_has_files "$source_path_black_usb"
+
+        count_files "$source_path_black_usb"
 
         number_of_files_in_src=$(find "${source_path_black_usb%/}" -type f | wc -l)
         echo "Number of files in source path: " "$number_of_files_in_src"
