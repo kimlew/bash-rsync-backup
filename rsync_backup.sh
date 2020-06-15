b480036c35cde398fb0ebba8063e8e9ec4d6e831
Kim Lew <kimlew451@gmail.com>
2020-06-15 15:42:35 -0600

    Adds global variables for USB & HD "nice" names & variable substitutions

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -8,8 +8,6 @@
 # when entering the source & destination/target.
 # - prevents creation of unwanted duplicated sub-directories.
 #
-# Author: Kim Lew
-
 # For SOURCE: State the specific sub-directory WITHOUT /.
 # For DESTINATION (target): Do NOT state specific sub-directory, BUT add a /.
 
@@ -20,6 +18,8 @@
 # Limitation: Only 2 USB ports on laptop, therefore:
 # To backup USB drive to Red HD: Connect USB drive, connect Red HD & run script with option 3.
 # To backup USB drive to Blue HD: Connect USB drive, connect Blue HD & run script with option 4.
+
+# Author: Kim Lew
 
 # Check for valid directory paths for source & destination.
 # -e - tests if a path exists without testing what type of file it is.
@@ -48,21 +48,8 @@ print_number_of_files() {
 
 do_backup_for_2_targets() {
   # For Case 1, Documents -> Red & Blue HDs & Case2, PHOTOS -> Red & Blue HDs.
-  # Note: Passes these arguments for Documents case, Case 1:
-  # source_path_Documents="/Users/kimlew/Documents"
-  # dest_red="/Volumes/ToshibaRD/"
-  # dest_blue="/Volumes/ToshibaBL/"
-  # e.g. do_backup_for_2_targets "$source_path_Documents" "Documents" "$dest_red" "Red_HD" "$dest_blue" "Blue_HD"
-  # e.g. do_backup_for_2_targets "$source_path_PHOTOS" "PHOTOS" "$dest_red" "Red_HD" "$dest_blue" "Blue_HD"
-
   local source_path=$1
   local source_name=$2
-  
-  local dest_path_red=$3
-  local dest_name_red=$4
-
-  local dest_path_blue=$5
-  local dest_name_blue=$6
   
   echo "BACKUP in progress..."
   echo
@@ -71,20 +58,22 @@ do_backup_for_2_targets() {
   # -v is verbose vs. -q, --quiet - to suppress non-error messages.
   # > dry-run_Documents_to_RedHD.txt \
   rsync -avi --progress --stats --exclude={'.DocumentRevisions-V100','.TemporaryItems','.Spotlight-V100','.Trashes','.fseventsd'} \
-  "$source_path" "$dest_path_red" \
-  > backup_"${source_name}"_to_"${dest_name_red}".txt \
-  && echo "BACKUP DONE of $source_name -> $dest_name_red."
+  "$source_path" "$dest_red" \
+  > backup_"${source_name}"_to_"${red_drive_name}".txt \
+  && echo "BACKUP DONE of $source_name -> $red_drive_name."
 
   rsync -avi --progress --stats --exclude={'.DocumentRevisions-V100','.TemporaryItems','.Spotlight-V100','.Trashes','.fseventsd'} \
-  "$source_path" "$dest_path_blue" \
-  > backup_"${source_name}"_to_"${dest_name_blue}".txt \
-  && echo "BACKUP DONE of $source_name -> $dest_name_blue."
+  "$source_path" "$dest_blue" \
+  > backup_"${source_name}"_to_"${blue_drive_name}".txt \
+  && echo "BACKUP DONE of $source_name -> $blue_drive_name."
   echo
 }
 do_backup_for_1_target() {
-  # For Case 3, Black USB -> Red HD & Case 4, Black USB -> Blue HD.
-  # e.g. do_backup_for_1_target "$source_path_black_usb" "Black_USB" "$dest_red" "Red_HD"
-  # e.g. do_backup_for_1_target "$source_path_black_usb" "Black_USB" "$dest_blue" "Blue_HD"
+  # For Case 3, Black USB -> Red HD & Case 4, Black USB -> Blue HD. Passes in:
+  # source_path_black_usb="/Volumes/Kingston16"
+  # dest_red="/Volumes/ToshibaRD/" OR dest_blue="/Volumes/ToshibaBL/"
+  # e.g. do_backup_for_1_target "$source_path_black_usb" "black_usb_name" "$dest_red" "red_drive_name"
+  # e.g. do_backup_for_1_target "$source_path_black_usb" "black_usb_name" "$dest_blue" "blue_drive_name"
   local source_path=$1
   local source_name=$2
   
@@ -126,7 +115,7 @@ MENU
     PS3="Which backup are you doing? Type 1 to 4, or 0 to quit: "
 
     # -r - interpret backslash as part of the line, NOT as escape char.
-    # -p - execute read using prompt
+    # -p - execute read using prompt.
     read -r -p "Type an option number. Or type 0 or Q to exit: " option
     echo
 
@@ -134,16 +123,19 @@ MENU
     # contents to destination/target. Prevents copying a repeated directory.
     # Specifically here, copies from last item in source path to target's root &
     # creates the directory, if it doesn't already exist, e.g., creates Documents
-    # directory under /Volumes/ToshibaRD/, if there isn't one already .
+    # directory under /Volumes/ToshibaRD/, if there isn't one already.
     source_path_Documents="/Users/kimlew/Documents"
-    # source_path_Documents="/Users/kimlew/Documents/computer_website_camera_info/Camera/Canon_Rebel_T3i_Esst_Train_2011"
     source_path_PHOTOS="/Users/kimlew/PHOTOS"
     source_path_black_usb="/Volumes/Kingston16"
-    # "/Volumes/Kingston16/test_King_to_ToshibaBL"
-    # "/Volumes/Kingston16/test_King_to_ToshibaRD"
     
     dest_red="/Volumes/ToshibaRD/"
     dest_blue="/Volumes/ToshibaBL/"
+
+    Documents_dir_name="Documents"
+    PHOTOS_dir_name="PHOTOS"
+    black_usb_name="Black_USB"
+    red_drive_name="Red_HD"
+    blue_drive_name="Blue_HD"
 
     time_start=$(date +%s)
 
@@ -167,11 +159,9 @@ MENU
         print_number_of_files "$dest_blue" "$num_of_files_in_dest_blue_before_backup" 
         echo
         
-        # Pass 6 arguments with call of function, do_backup().
-        # Could also do this way:
+        # Pass arguments with paths & "nice" names with function call, e.g.,
         # do_backup "$source_path_Documents" "nicename1" "$dest_red" "$nicename1"
-        # do_backup "$source_path_Documents" "nicename1" "$dest_blue" "$nicename2"
-        do_backup_for_2_targets "$source_path_Documents" "Documents" "$dest_red" "Red_HD" "$dest_blue" "Blue_HD"
+        do_backup_for_2_targets "$source_path_Documents" "$Documents_dir_name"
 
         echo "AFTER BACKUP: "
         num_of_files_in_dest_red_after_backup=$(count_files_dirs_etc "$dest_red")
@@ -181,9 +171,9 @@ MENU
 
         transferred_files_dirs_to_red=$((num_of_files_in_dest_red_after_backup - num_of_files_in_dest_red_before_backup))
         transferred_files_dirs_to_blue=$((num_of_files_in_dest_blue_after_backup - num_of_files_in_dest_blue_before_backup))
-        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_Documents_to_RedHD.txt | sed -E 's/^.*transferred: //')
-        updated_files_dirs_to_blue=$(grep '^Number of files transferred' backup_Documents_to_BlueHD.txt | sed -E 's/^.*transferred: //')
-        
+        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_"${Documents_dir_name}"_to_"${red_drive_name}".txt | sed -E 's/^.*transferred: //')
+        updated_files_dirs_to_blue=$(grep '^Number of files transferred' backup_"${Documents_dir_name}"_to_"${blue_drive_name}".txt | sed -E 's/^.*transferred: //')
+
         echo "New files transferred to $dest_red: " "$transferred_files_dirs_to_red"
         echo "New files transferred to $dest_blue: " "$transferred_files_dirs_to_blue"
         echo "Updated files to $dest_red: " "$updated_files_dirs_to_red"
@@ -212,7 +202,7 @@ MENU
         print_number_of_files "$dest_blue" "$num_of_files_in_dest_blue_before_backup" 
         echo
 
-        do_backup_for_2_targets "$source_path_PHOTOS" "PHOTOS" "$dest_red" "Red_HD" "$dest_blue" "Blue_HD"
+        do_backup_for_2_targets "$source_path_PHOTOS" "$PHOTOS_dir_name"
 
         echo "AFTER BACKUP: "
         num_of_files_in_dest_red_after_backup=$(count_files_dirs_etc "$dest_red")
@@ -222,9 +212,9 @@ MENU
 
         transferred_files_dirs_to_red=$((num_of_files_in_dest_red_after_backup - num_of_files_in_dest_red_before_backup))
         transferred_files_dirs_to_blue=$((num_of_files_in_dest_blue_after_backup - num_of_files_in_dest_blue_before_backup))
-        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_PHOTOS_to_RedHD.txt | sed -E 's/^.*transferred: //')
-        updated_files_dirs_to_blue=$(grep '^Number of files transferred' backup_PHOTOS_to_BlueHD.txt | sed -E 's/^.*transferred: //')
-        
+        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_"${PHOTOS_dir_name}"_to_"${red_drive_name}".txt | sed -E 's/^.*transferred: //')
+        updated_files_dirs_to_blue=$(grep '^Number of files transferred' backup_"${PHOTOS_dir_name}"_to_"${blue_drive_name}".txt | sed -E 's/^.*transferred: //')
+
         echo "New files transferred to $dest_red: " "$transferred_files_dirs_to_red"
         echo "New files transferred to $dest_blue: " "$transferred_files_dirs_to_blue"
         echo "Updated files to $dest_red: " "$updated_files_dirs_to_red"
@@ -252,14 +242,14 @@ MENU
         print_number_of_files "$dest_red" "$num_of_files_in_dest_red_before_backup"
         echo
         
-        do_backup_for_1_target "$source_path_black_usb" "Black_USB" "$dest_red" "Red_HD"
+        do_backup_for_1_target "$source_path_black_usb" "$black_usb_name" "$dest_red" "$red_drive_name"
 
         echo "AFTER BACKUP: "
         num_of_files_in_dest_red_after_backup=$(count_files_dirs_etc "$dest_red")
         print_number_of_files "$dest_red" "$num_of_files_in_dest_red_after_backup"
 
         transferred_files_dirs_to_red=$((num_of_files_in_dest_red_after_backup - num_of_files_in_dest_red_before_backup))
-        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_Black_USB_to_Red_HD.txt | sed -E 's/^.*transferred: //')
+        updated_files_dirs_to_red=$(grep '^Number of files transferred' backup_"${black_usb_name}"_to_"${red_drive_name}".txt | sed -E 's/^.*transferred: //')
 
         echo "New files transferred to $dest_red: " "$transferred_files_dirs_to_red"
         echo "Updated files to $dest_red: " "$updated_files_dirs_to_red"
@@ -282,8 +272,6 @@ MENU
         print_number_of_files "$dest_blue" "$num_of_files_in_dest_blue_before_backup"
         echo
         
-        black_usb_name="Black_USB"
-        blue_drive_name="Blue_HD"
         do_backup_for_1_target "$source_path_black_usb" "$black_usb_name" "$dest_blue" "$blue_drive_name"
 
         echo "AFTER BACKUP: "
